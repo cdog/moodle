4e2b129c66bdb1251ed7222c54ca3699dcf3d313
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-03-28 17:14:55 +0200

    Added the message tray

--- theme/bootstrapbase/layout/columns2.php
+++ theme/bootstrapbase/layout/columns2.php
@@ -56,6 +56,7 @@ echo $OUTPUT->doctype() ?>
                 <span class="icon-bar"></span>
             </a>
             <?php echo $OUTPUT->user_menu(); ?>
+            <?php echo $OUTPUT->message_tray(); ?>
             <div class="nav-collapse collapse">
                 <?php echo $OUTPUT->custom_menu(); ?>
                 <ul class="nav pull-right">
